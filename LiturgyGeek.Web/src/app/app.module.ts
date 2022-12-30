7223f5364c362cbb1fb1d8b150b31fd0b03cd40d
kaleb70 <kaleb70@gmail.com>
2022-12-30 12:36:16 -0600

    consolidate calendar routings to avoid unwanted refresh

--- LiturgyGeek.Web/src/app/app.module.ts
+++ LiturgyGeek.Web/src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes, UrlMatchResult, UrlSegment } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 
+const calendarMatcher = (segments: UrlSegment[]): UrlMatchResult => {
+  if (segments.length >= 1 && segments[0].path == 'calendar') {
+    switch (segments.length) {
+      case 1:
+        return {
+          consumed: segments,
+          posParams: {}
+        }
+      case 2:
+        return {
+          consumed: segments,
+          posParams: { calendarKey: segments[1] }
+        }
+      case 4:
+        return {
+          consumed: segments,
+          posParams: {
+            calendarKey: segments[1],
+            year: segments[2],
+            month: segments[3]
+          }
+        }
+    }
+  }
+  return <UrlMatchResult>(null as any);
+}
+
 const routes: Routes = [
-  { path: 'calendar', component: CalendarComponent },
-  { path: 'calendar/:calendarKey/:year/:month', component: CalendarComponent },
+  { matcher: calendarMatcher, component: CalendarComponent },
   { path: '', redirectTo: '/calendar', pathMatch: 'full' },
 ]
 
