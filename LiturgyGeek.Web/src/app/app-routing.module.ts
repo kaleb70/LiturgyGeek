a0651524bba050b5e4d0dabf57b1639beb9dcfb5
kaleb70 <kaleb70@gmail.com>
2022-12-31 14:33:51 -0600

    day view

--- LiturgyGeek.Web/src/app/app-routing.module.ts
+++ LiturgyGeek.Web/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, UrlMatchResult, UrlSegment } from '@angular/router'
 
 import { MonthComponent } from './month/month.component';
+import { DayComponent } from './day/day.component';
 
 const monthMatcher = (segments: UrlSegment[]): UrlMatchResult => {
 
@@ -38,8 +39,37 @@ const monthMatcher = (segments: UrlSegment[]): UrlMatchResult => {
   return <UrlMatchResult>(null as any);
 }
 
+const dayMatcher = (segments: UrlSegment[]): UrlMatchResult => {
+
+  if (segments.length > 2
+    && segments[0].path.toLowerCase() == "calendar"
+    && segments[2].path.toLowerCase() == "day") {
+
+    switch (segments.length) {
+      case 3:
+        return {
+          consumed: segments,
+          posParams: { calendarKey: segments[1] }
+        };
+
+      case 6:
+        return {
+          consumed: segments,
+          posParams: {
+            calendarKey: segments[1],
+            year: segments[3],
+            month: segments[4],
+            day: segments[5]
+          }
+        }
+    }
+  }
+  return <UrlMatchResult>(null as any);
+}
+
 const routes: Routes = [
   { matcher: monthMatcher, component: MonthComponent },
+  { matcher: dayMatcher, component: DayComponent},
   { path: '', redirectTo: '/calendar', pathMatch: 'full' },
 ]
 
--- LiturgyGeek.Web/src/app/month/month.component.ts
+++ LiturgyGeek.Web/src/app/month/month.component.ts
@@ -2,6 +2,7 @@ import { AfterViewChecked, Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router'
 import { Tooltip } from 'bootstrap';
+import 'src/lib/date.extensions';
 
 @Component({
   selector: 'app-calendar',
@@ -13,33 +14,40 @@ export class MonthComponent implements AfterViewChecked {
   public year: number = 0;
   public month: number = 0;
 
+  public isDefaultDate: boolean = false;
+
+  private dateShown: Date = new Date();
+  private today: Date = new Date().asDateOnly();
+
+  public prevMonth: Date = this.today.addMonths(-1);
+  public nextMonth: Date = this.today.addMonths(1);
+
   public result?: CalendarMonth;
 
   private tooltipsInvalid: boolean;
 
   constructor(http: HttpClient, route: ActivatedRoute) {
-    //const now = new Date();
-    ////http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-    //this.calendarKey = route.snapshot.paramMap.get('calendarKey') ?? 'oca';
-    //this.year = +(route.snapshot.paramMap.get('year') ?? now.getFullYear());
-    //this.month = +(route.snapshot.paramMap.get('month') ?? now.getMonth() + 1);
+
     this.tooltipsInvalid = false;
 
     route.params.subscribe(params => {
-      const now = new Date();
+      this.today = new Date().asDateOnly();
+
       this.calendarKey = params['calendarKey'] ?? 'oca';
-      this.year = +(params['year'] ?? now.getFullYear());
-      this.month = +(params['month'] ?? now.getMonth() + 1);
+      this.year = +(params['year'] ?? this.today.getFullYear());
+      this.month = +(params['month'] ?? this.today.getMonth() + 1);
+
+      this.isDefaultDate = params['year'] == null;
+
+      this.dateShown = new Date(this.year, this.month - 1, 1);
+      this.prevMonth = this.dateShown.addMonths(-1);
+      this.nextMonth = this.dateShown.addMonths(1);
 
       http.get<CalendarMonth>(`https://localhost:7246/Calendar/${this.calendarKey}/${this.year}/${this.month}`).subscribe(result => {
         this.result = result;
         this.tooltipsInvalid = true;
       }, error => console.error(error));
     });
-
-    //http.get<CalendarMonth>(`https://localhost:7246/Calendar/${this.calendarKey}/${this.year}/${this.month}`).subscribe(result => {
-    //  this.result = result;
-    //}, error => console.error(error));
   }
   ngAfterViewChecked(): void {
     if (this.tooltipsInvalid) {
@@ -52,9 +60,7 @@ export class MonthComponent implements AfterViewChecked {
   }
 
   getDateClasses(day: CalendarDaySummary): string {
-    const now = new Date();
-
-    if (day.year == now.getFullYear() && day.month == now.getMonth() + 1 && day.day == now.getDate())
+    if (new Date(day.year, day.month - 1, day.day).equals(this.today))
       return "calendar-cell-today";
 
     if (day.year != this.year || day.month != this.month)
@@ -63,6 +69,14 @@ export class MonthComponent implements AfterViewChecked {
     return "";
   }
 
+  getDayRouterLink(day: CalendarDaySummary): string {
+
+    var result = `/calendar/${this.calendarKey}/day`;
+    if (!(this.isDefaultDate && new Date(day.year, day.month - 1, day.day).equals(this.today)))
+      result += `/${day.year}/${day.month}/${day.day}`;
+
+    return result;
+  }
   title = 'LiturgyGeek.Web';
 }
 
