3a64c5e64ffd81277ae41418e7799305e89afeb6
kaleb70 <kaleb70@gmail.com>
2022-12-30 19:22:48 -0600

    tooltips

--- LiturgyGeek.Web/src/app/calendar/calendar.component.ts
+++ LiturgyGeek.Web/src/app/calendar/calendar.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router'
+import { Tooltip } from 'bootstrap';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements AfterViewChecked {
   public calendarKey: string = "";
   public year: number = 0;
   public month: number = 0;
 
   public result?: CalendarMonth;
+
+  private tooltipsInvalid: boolean;
 
   constructor(http: HttpClient, route: ActivatedRoute) {
     //const now = new Date();
@@ -20,6 +23,7 @@ export class CalendarComponent {
     //this.calendarKey = route.snapshot.paramMap.get('calendarKey') ?? 'oca';
     //this.year = +(route.snapshot.paramMap.get('year') ?? now.getFullYear());
     //this.month = +(route.snapshot.paramMap.get('month') ?? now.getMonth() + 1);
+    this.tooltipsInvalid = false;
 
     route.params.subscribe(params => {
       const now = new Date();
@@ -29,12 +33,22 @@ export class CalendarComponent {
 
       http.get<CalendarMonth>(`https://localhost:7246/Calendar/${this.calendarKey}/${this.year}/${this.month}`).subscribe(result => {
         this.result = result;
+        this.tooltipsInvalid = true;
       }, error => console.error(error));
     });
 
     //http.get<CalendarMonth>(`https://localhost:7246/Calendar/${this.calendarKey}/${this.year}/${this.month}`).subscribe(result => {
     //  this.result = result;
     //}, error => console.error(error));
+  }
+  ngAfterViewChecked(): void {
+    if (this.tooltipsInvalid) {
+      var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+      var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl);
+      });
+      this.tooltipsInvalid = false;
+    }
   }
 
   getDateClasses(day: CalendarDaySummary): string {
