de7009602c9a7291967cf4791451aaa40fa9eb34
kaleb70 <kaleb70@gmail.com>
2022-12-31 10:33:07 -0600

    use a more standard project structure for routing

--- LiturgyGeek.Web/src/app/app.module.ts
+++ LiturgyGeek.Web/src/app/app.module.ts
@@ -1,57 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes, UrlMatchResult, UrlSegment } from '@angular/router'
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { MonthComponent } from './month/month.component';
-
-const monthMatcher = (segments: UrlSegment[]): UrlMatchResult => {
-
-  if (segments.length > 0
-      && segments[0].path.toLowerCase() == "calendar"
-      && (segments.length <= 2 || segments[2].path.toLowerCase() == "month")) {
-
-    switch (segments.length) {
-      case 1:
-        return {
-          consumed: segments,
-          posParams: {}
-        };
-
-      case 2:
-      case 3:
-        return {
-          consumed: segments,
-          posParams: { calendarKey: segments[1] }
-        };
-
-      case 5:
-        return {
-          consumed: segments,
-          posParams: {
-            calendarKey: segments[1],
-            year: segments[3],
-            month: segments[4]
-          }
-        }
-    }
-  }
-  return <UrlMatchResult>(null as any);
-}
-
-const routes: Routes = [
-  { matcher: monthMatcher, component: MonthComponent },
-  { path: '', redirectTo: '/calendar', pathMatch: 'full' },
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonthComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, RouterModule.forRoot(routes)
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
